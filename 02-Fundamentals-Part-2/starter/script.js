7ed540da4309f1d10828788d64f38a4d019f048d
gdecaires <gdecaires@gmail.com>
2022-10-27 20:02:06 -0400

    working on arrays challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -136,3 +136,15 @@ HINT: Remember that an array needs a value in each position, and that value can 
 
 GOOD LUCK 😀
 */
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15
+    } else {
+        return bill * 0.20
+    };
+};
+
+const bills = [125, 555, 44];
+
+const tips = Array[calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])];
+console.log(tips);
