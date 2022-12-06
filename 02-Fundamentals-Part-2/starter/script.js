8bb173a203f4ba48380b6901be177a9dc149fd5d
gdecaires <gdecaires@gmail.com>
2022-12-06 19:54:03 -0400

    challenge 4

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -316,9 +316,3 @@ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const calcTip = function () {
     return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
 }
-
-for (let i = 0; i < bills.length; i++) {
-    let tips = calcTip(bills[i]);
-    }
-
- 
